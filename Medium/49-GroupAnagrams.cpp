5fb031f00edd6e86fcd54d417c5a8e5711f68711
Anjali <anjaliiverma09@gmail.com>
2025-11-21 20:52:10 +0530

    add 49 - Group Anagrams solution

--- Medium/49-GroupAnagrams.cpp
+++ Medium/49-GroupAnagrams.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> ans;
+        unordered_map<string, vector<string>> mp;
 
         for(string i: strs){
             string key = i;
             sort(key.begin(), key.end());
-            ans[key].push_back(i);
+            mp[key].push_back(i);
         }
 
         vector<vector<string>> result;
-        for(auto i: ans)
+        for(auto i: mp)
             result.push_back(i.second);
 
         return result;
